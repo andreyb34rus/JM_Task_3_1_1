590b5257bd246bd0362a5bed514f71fd2bfd8108
andreyb34rus <andreyb34rus@gmail.com>
2021-11-08 08:45:52 +0300

    fix not used imports

--- src/main/java/com/andreyb34rus/JM_Task_3_1_1/model/User.java
+++ src/main/java/com/andreyb34rus/JM_Task_3_1_1/model/User.java
@@ -4,11 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
